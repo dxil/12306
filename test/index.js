e735c2a5d90d8e5fdf50d4195e2e8eb290d2f109
cuzz <775046211@qq.com>
2018-05-24 23:47:26 +0800

    Init

---
+++ test/index.js
@@ -0,0 +1,43 @@
+const stations = require('../lib/station_name')
+
+function init () {
+  initStation ()
+}
+
+function initStation () {
+  let result = {}
+  let station_arr = []
+
+  station_arr = stations.station_names.split('@')
+  station_arr.splice(0, 1)
+  station_arr.forEach(val => {
+    let key = val.split('|')[1]
+    result[key] = val
+  })
+  console.log(result);  
+}
+
+function parseResult (res) {
+  let data_list = res.split('|')
+  let list = {}
+
+  list.from_station_code = data_list[6]
+  list.to_station_code = data_list[7]
+  list.from_station_name = ''
+  list.to_station_name = ''
+  list.start_time = data_list[8]
+  list.arrive_time = data_list[9]
+  list.time_duration = data_list[10]
+  list.business_seat = data_list[32] || '--'
+  list.first_class_seat = data_list[31] || '--'
+  list.second_class_seat = data_list[30] || '--'
+  list.pneumatic_sleep = data_list[33] || '--'
+  list.soft_sleep = data_list[23] || '--'
+  list.hard_sleep = data_list[28] || '--'
+  list.hard_seat = data_list[29] || '--'
+  list.no_seat = data_list[26] || '--'
+
+  return list
+}
+
+init()
---
+++ vue/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui'
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
